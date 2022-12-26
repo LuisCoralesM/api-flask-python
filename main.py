93022334ac2c9f394e1b2af81614b626ee6f761f
Luis Corales <lcorales@stackbuilders.com>
2022-12-26 16:37:29 -0500

    First example

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 app = Flask("PokeApi")
 api = Api(app)
 
+parser = reqparse.RequestParser()
+parser.add_argument("name", required=True)
+
+pokemon = {
+    "1": {"name": "Charizard", "type": "Fire/Flying"},
+    "2": {"name": "Blastoise", "type": "Water"},
+    "3": {"name": "Venasaur", "type": "Poison/Grass"},
+}
+
 
 class Pokemon(Resource):
-    def get(self):
-        return "Charizard"
+    def get(self, id):
+        return pokemon[id]
 
 
-api.add_resource(Pokemon, "/")
+api.add_resource(Pokemon, "/<id>")
+
 
 if __name__ == "__main__":
     app.run()
